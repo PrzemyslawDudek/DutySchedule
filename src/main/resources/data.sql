d896c5ec5149fb72795a7aec95c923b3adedc7da
Przemek <dudek.mail@mail.com>
2018-08-12 15:14:14 +0200

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO break(id, break_number, break_length, break_time) VALUE (1, 0, 10, '7:45 - 8:00');
+INSERT INTO break(id, break_number, break_length, break_time) VALUE (1, 1, 10, '8:45 - 8:55');
+INSERT INTO break(id, break_number, break_length, break_time) VALUE (1, 2, 10, '9:40 - 9:50');
+
+INSERT INTO place_of_guard(id, name, is_assigned) VALUE (1, 'place1', 0);
+INSERT INTO place_of_guard(id, name, is_assigned) VALUE (2, 'place2', 0);
+INSERT INTO place_of_guard(id, name, is_assigned) VALUE (3, 'place3', 0);
+
+INSERT INTO teacher(id, name, surname) VALUE (1, 'Jan', 'Kowalski');
+INSERT INTO teacher(id, name, surname) VALUE (2, 'Piotr', 'Nowak');
+INSERT INTO teacher(id, name, surname) VALUE (3, 'Adam', 'Cybulski');
